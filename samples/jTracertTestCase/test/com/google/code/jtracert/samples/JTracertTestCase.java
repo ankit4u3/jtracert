6a2d7559159c6eb70fcccf89589fe377343f375d
Dmitry.Bedrin <Dmitry.Bedrin@03512b4a-554d-0410-9ef6-ff3f2f52b5f1>
2009-01-26 10:48:43 +0000

    Integration tests refactoring

--- samples/jTracertTestCase/test/com/google/code/jtracert/samples/JTracertTestCase.java
+++ samples/jTracertTestCase/test/com/google/code/jtracert/samples/JTracertTestCase.java
@@ -2,8 +2,42 @@ package com.google.code.jtracert.samples;
 
 import junit.framework.TestCase;
 
-public class JTracertTestCase extends TestCase {
+import java.io.IOException;
+import java.io.File;
+import java.io.InputStream;
 
+public abstract class JTracertTestCase extends TestCase {
+
+    protected Process startJavaProcessWithJTracert(String jarFileName) throws IOException {
+
+        String[] commands = new String[]{
+                "java",
+                "-DanalyzerOutput=serializableTcpClient",
+                "-javaagent:../../deploy/jTracert.jar",
+                "-jar",jarFileName
+        };
+
+        ProcessBuilder processBuilder = new ProcessBuilder(commands);
+        processBuilder.redirectErrorStream(true);
+        File directory = new File(".").getAbsoluteFile();
+        processBuilder.directory(directory);
+
+        Process process = processBuilder.start();
+
+        InputStream is = process.getInputStream();
+        for(int i = is.read(); i != -1; i = is.read())
+        {
+            System.out.print((char)i);
+        }
+
+        return process;
+
+    }
+
+    protected JTracertSerializableTcpServer startJTracertTcpServer(int port) {
+        JTracertSerializableTcpServer tcpServer = new JTracertSerializableTcpServer(port);
+        tcpServer.start();
+        return tcpServer;
+    }
     
-
 }

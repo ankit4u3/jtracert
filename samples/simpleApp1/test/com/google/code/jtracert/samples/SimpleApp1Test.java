f6b31f70001fdc472c3943299c9055ef662eda35
Dmitry.Bedrin <Dmitry.Bedrin@03512b4a-554d-0410-9ef6-ff3f2f52b5f1>
2009-01-26 10:21:43 +0000

    Added jTracertTestCase - a subproject for easily adding new integration tests (samples actually) to the  project.

--- samples/simpleApp1/test/com/google/code/jtracert/samples/SimpleApp1Test.java
+++ samples/simpleApp1/test/com/google/code/jtracert/samples/SimpleApp1Test.java
@@ -4,61 +4,18 @@ import junit.framework.TestCase;
 import org.junit.Test;
 
 import java.io.*;
-import java.util.Queue;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 import com.google.code.jtracert.model.MethodCall;
 
 public class SimpleApp1Test extends TestCase {
 
-    private static class SerializableTcpServer implements Runnable {
-
-        private Queue<MethodCall> methodCallsQueue = new ArrayBlockingQueue<MethodCall>(1);
-
-        public void run() {
-            try {
-                ServerSocket serverSocket = new ServerSocket(60002);
-                Socket socket = serverSocket.accept();
-
-                InputStream inputStream = socket.getInputStream();
-                ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
-                Object object = objectInputStream.readObject();
-
-                MethodCall methodCall = (MethodCall) object;
-
-                methodCallsQueue.offer(methodCall);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        public MethodCall getMethodCall() {
-            return methodCallsQueue.remove();
-        }
-
-    }
-
     @Test
     public void testSimpleApp1() throws Exception {
 
-        SerializableTcpServer tcpServer = new SerializableTcpServer();
-        Thread t = new Thread(tcpServer);
-        t.start();
+        JTracertSerializableTcpServer tcpServer = new JTracertSerializableTcpServer(60002);
+        tcpServer.start();
 
-        String[] commands = new String[]{
-                "java",
-                "-DanalyzerOutput=serializableTcpClient",
-                "-javaagent:../../deploy/jTracert.jar",
-                "-jar","deploy/simpleApp1.jar"
-        };
-
-        ProcessBuilder processBuilder = new ProcessBuilder(commands);
-        processBuilder.redirectErrorStream(true);
-        processBuilder.directory(new File(".").getAbsoluteFile());
-
-        Process process = processBuilder.start();
+        Process process = startJavaProcessWithJTracert("deploy/simpleApp1.jar");
 
         int exitCode = process.waitFor();
 
@@ -68,6 +25,22 @@ public class SimpleApp1Test extends TestCase {
 
         assertNotNull(methodCall);
 
+    }
+
+    private Process startJavaProcessWithJTracert(String jarFileName) throws IOException {
+
+        String[] commands = new String[]{
+                "java",
+                "-DanalyzerOutput=serializableTcpClient",
+                "-javaagent:../../deploy/jTracert.jar",
+                "-jar",jarFileName
+        };
+
+        ProcessBuilder processBuilder = new ProcessBuilder(commands);
+        processBuilder.redirectErrorStream(true);
+        processBuilder.directory(new File(".").getAbsoluteFile());
+
+        return processBuilder.start();
     }
 
 }

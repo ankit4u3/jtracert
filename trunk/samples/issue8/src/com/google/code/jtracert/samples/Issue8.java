82027a8e7b3c4c197b4d5345533af5750fdadc47
Dmitry.Bedrin <Dmitry.Bedrin@03512b4a-554d-0410-9ef6-ff3f2f52b5f1>
2009-02-04 09:21:20 +0000

    Added test case for Issue 8

---
+++ trunk/samples/issue8/src/com/google/code/jtracert/samples/Issue8.java
@@ -0,0 +1,34 @@
+package com.google.code.jtracert.samples;
+
+class JMSManager {
+
+    public void createConnection() {}
+    public void sendMessage(String message) {}
+    public void close() {}
+
+}
+
+public class Issue8 {
+
+    public static void main(String[] arguments) throws Exception {
+        method1();
+        method2();
+    }
+
+    private static void method1() throws Exception {
+        JMSManager manager = new JMSManager();
+        manager.createConnection();
+        manager.sendMessage("hello, world");
+        manager.close();
+    }
+
+    private static void method2()  {
+        try {
+            JMSManager manager = new JMSManager();
+            manager.createConnection();
+            manager.sendMessage("hello, world");
+            manager.close();
+        }catch(Exception e){e.printStackTrace();}
+    }
+
+}

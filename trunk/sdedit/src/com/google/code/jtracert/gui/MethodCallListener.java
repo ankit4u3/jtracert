c59817fa34660aa53ef5b06af036a7b0632dbc05
Dmitry.Bedrin <Dmitry.Bedrin@03512b4a-554d-0410-9ef6-ff3f2f52b5f1>
2009-02-24 14:43:31 +0000

    Removed sdedit sources; sdedit precompiled jar added to SVN

--- trunk/sdedit/src/com/google/code/jtracert/gui/MethodCallListener.java
+++
@@ -1,9 +0,0 @@
-package com.google.code.jtracert.gui;
-
-import java.util.EventListener;
-
-public interface MethodCallListener extends EventListener {
-
-    void onMethodCall(MethodCallEvent methodCallEvent);
-
-}

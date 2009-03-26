fca26b845b22dabc14f3bdaa286d1804a34dbfcf
Dmitry.Bedrin <Dmitry.Bedrin@03512b4a-554d-0410-9ef6-ff3f2f52b5f1>
2009-03-26 10:24:23 +0000

    Brand new gui implementation started - investigating ways to implement sequence diagram control

---
+++ gui/src/com/google/code/jtracert/client/gui/shapes/MethodCallShape.java
@@ -0,0 +1,59 @@
+package com.google.code.jtracert.client.gui.shapes;
+
+import java.awt.*;
+
+public class MethodCallShape extends DiagramElement {
+
+    protected String methodName;
+
+    protected int captionHeight;
+
+    @Override
+    public void paint(Graphics g) {
+
+        g.setColor(active ? new Color(0xFF,0,0) : new Color(0,0,0));
+
+        g.drawString(
+                methodName,
+                x + 5,
+                y + captionHeight - 2
+        );
+
+        g.drawLine(
+                x,
+                y + captionHeight,
+                x + width,
+                y + captionHeight);
+
+        g.drawLine(
+                x + width - 3,
+                y + captionHeight - 3,
+                x + width,
+                y + captionHeight
+        );
+
+        g.drawLine(
+                x + width - 3,
+                y + captionHeight + 3,
+                x + width,
+                y + captionHeight
+        );
+
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public int getCaptionHeight() {
+        return captionHeight;
+    }
+
+    public void setCaptionHeight(int captionHeight) {
+        this.captionHeight = captionHeight;
+    }
+}

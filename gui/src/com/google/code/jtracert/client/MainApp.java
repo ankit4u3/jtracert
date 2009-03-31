9980daf42d9f8aadc8c9095d139a68dc588a3c5f
Dmitry.Bedrin <Dmitry.Bedrin@03512b4a-554d-0410-9ef6-ff3f2f52b5f1>
2009-03-31 13:06:42 +0000

    JSequenceDiagram control implementation

--- gui/src/com/google/code/jtracert/client/MainApp.java
+++ gui/src/com/google/code/jtracert/client/MainApp.java
@@ -1,8 +1,6 @@
 package com.google.code.jtracert.client;
 
-import com.google.code.jtracert.client.gui.JSequenceDiagramPane;
 import com.google.code.jtracert.sequenceDiagram.DefaultSequenceDiagramModel;
-import com.google.code.jtracert.sequenceDiagram.SequenceDiagramModel;
 import com.google.code.jtracert.sequenceDiagram.JSequenceDiagram;
 import com.google.code.jtracert.client.model.MethodCallImpl;
 
@@ -39,43 +37,6 @@ public class MainApp {
         mainFrameContentPane.add(jSequenceDiagram);
 
         mainFrame.setVisible(true);
-
-/*        JFrame mainFrame = new JFrame();
-
-        mainFrame.setSize(640,480);
-        mainFrame.setTitle("jTracert Client");
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        Container mainFrameContentPane = mainFrame.getContentPane();
-
-        SequenceDiagramModel sequenceDiagramModel = new DefaultSequenceDiagramModel();
-
-
-        MethodCallImpl mc1 = new MethodCallImpl("Class1","");
-        com.google.code.jtracert.sequenceDiagram.MethodCall methodCall = mc1;
-
-
-        MethodCallImpl mc2 = new MethodCallImpl("Class2","method2longName");
-
-        mc1.addCallee(mc2);
-
-        mc1 = new MethodCallImpl("Class3","method3");
-        mc2.addCallee(mc1);
-
-        mc1 = new MethodCallImpl("Class3","foo");
-        mc2.addCallee(mc1);
-
-        mc1 = new MethodCallImpl("Class3","bar");
-        mc2.addCallee(mc1);
-
-        sequenceDiagramModel.setRootMethodCall(methodCall);
-
-        JSequenceDiagramPane jSequenceDiagramPane = new JSequenceDiagramPane();
-        jSequenceDiagramPane.setModel(sequenceDiagramModel);
-
-        mainFrameContentPane.add(jSequenceDiagramPane);
-
-        mainFrame.setVisible(true);*/
 
     }
 

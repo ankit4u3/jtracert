95ba33a5a193d7453df42b67344974544e600008
Dmitry.Bedrin <Dmitry.Bedrin@03512b4a-554d-0410-9ef6-ff3f2f52b5f1>
2009-09-04 06:40:15 +0000

    Migration to Maven rollback

--- jtracert-deployment/src/main/bin/jtracert.cmd
+++
@@ -1,8 +0,0 @@
-rem
-rem jTracert GUI start script
-rem
-rem Distributed under GNU GENERAL PUBLIC LICENSE Version 3
-rem Author Dmitry.Bedrin@gmail.com
-rem
-
-"%JAVA_HOME%\bin\java" -jar jTracert-gui.jar
